4affffbbb3c061aa127425874e490d99b7767b31
oskarszura <oskar.szura@gmail.com>
2015-11-02 22:53:44 +0100

    Issue #2 - Implement UI file dropper component
    Implement fileDropper component scaffolding, attach onDrop handler callback.

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,27 +1,6 @@
 const React = require('react')
     ,reactDom = require('react-dom')
-    ,fileDropper = require('./components/fileDropper.jsx');
-
-var CommentBox = React.createClass({
-    render: function() {
-        return (
-            <div className="commentBox">
-                Hello, world! I am a CommentBox.
-            </div>
-        );
-    }
-});
-
-var FileDropper = React.createClass({
-    render: function() {
-        return (
-            <div className="component-file-dropper">
-                Drop files here...
-            </div>
-        );
-    }
-});
-
+    ,FileDropper = require('./components/fileDropper.jsx');
 
 reactDom.render(
     <FileDropper />,
--- public/js/components/fileDropper.jsx
+++ public/js/components/fileDropper.jsx
@@ -1,9 +1,18 @@
 const React = require('react');
 
 var FileDropper = React.createClass({
+    allowDrop: function (e) {
+        e.preventDefault();
+    },
+
+    handleOnDrop: function(e) {
+        e.preventDefault();
+        console.log(e)
+    },
+
     render: function() {
         return (
-            <div className="component-file-dropper">
+            <div className="component-file-dropper" onDrop={this.handleOnDrop} onDragOver={this.allowDrop}>
                 Drop files here...
             </div>
         );
