3f307170f3c353d9e2e75b0645db06f556698c5a
oskarszura <oskar.szura@gmail.com>
2015-11-28 18:19:55 +0100

    Issue #4 - [BACKEND] HTTPS handling
    Add certificates.
    
    Implement https server with the given keys and certs.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express'),
-    config = require('./config/config');
+    https = require('https'),
+    http = require('http'),
+    fs = require('fs')
+    config = require('./config/config'),
+    hskey = fs.readFileSync('hacksparrow-key.pem'),
+    hscert = fs.readFileSync('hacksparrow-cert.pem'),
+    options = {
+      key: hskey,
+      cert: hscert
+    };
 
 var app = express();
 
 require('./config/express')(app, config);
 
-app.listen(config.port);
+http.createServer(app).listen(config.port);
+https.createServer(options, app).listen(3500);
