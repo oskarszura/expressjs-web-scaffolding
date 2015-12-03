a2b24960344b330ad7d87f326c0d733b958696e8
oskarszura <oskar.szura@gmail.com>
2015-12-03 19:43:00 +0100

    Issue #5 - [UI] UI router
    Moved all the rendering from the backend to the client part.

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,18 +1,14 @@
 const React = require('react')
     , reactDom = require('react-dom')
-    , FileDropper = require('./components/fileDropper.jsx')
     , Router = require('./components/router.jsx');
 
-reactDom.render(
-    <FileDropper />,
-    document.querySelector('.js-file-dropper')
-);
+function handleNewHash () {
 
+  var location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/')
+    , application = <Router location={location} />;
 
-function handleNewHash() {
-  var location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
-  var application = <Router location={location} />;
-  reactDom.render(application, document.getElementsByTagName('body')[0]);
+  reactDom.render(application, document.getElementById('app'));
+
 }
 
 handleNewHash()
--- public/js/components/router.jsx.js
+++ public/js/components/router.jsx.js
@@ -1,15 +1,38 @@
 const React = require('react')
   , $ = require('jquery')
+  , reactDom = require('react-dom')
+  , FileDropper = require('./fileDropper.jsx')
 
   , Router = React.createClass({
 
-    render: function() {
+    render: function () {
 
       switch (this.props.location[0])  {
         case '':
-          return <div><h1>Index Page</h1></div>;
+
+          return <div class="row">
+                  <div class="col-sm-12">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <h1>Title</h1>
+                          <p>Welcome...</p>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-12">
+
+                          You are logged in - congrats !!!
+
+                          <FileDropper />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                 </div>;
 
         default:
+
           return <div><h1>Not Found</h1></div>;
       }
 
