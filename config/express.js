8522880acba85dc1cd276bc6b178d85c6e319a81
oskarszura <oskar.szura@gmail.com>
2016-03-29 22:02:12 +0200

    Make home page accesible. Fix user flow.

--- config/express.js
+++ config/express.js
@@ -52,6 +52,8 @@ module.exports = (app, config) => {
   app.use((req, res, next) => {
       if (req.path == '/login'
           || req.path == '/login/register'
+          || req.path == '/'
+          || req.path == '/api/article'
           || req.isAuthenticated()) {
           //|| true) { // @TODO - remove
           next();
