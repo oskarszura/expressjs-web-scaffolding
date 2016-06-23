eac61496426d52ca6801031da5403aa9d445a1be
oskarszura <oskar.szura@gmail.com>
2016-06-23 08:57:01 +0200

    Node v6.2.2. Conditional logging only on dev.

--- app.js
+++ app.js
@@ -17,26 +17,31 @@ const express = require('express')
   , workers = os.cpus().length
   , app = express()
   , expressApp = require('./config/express')(app, config)
+  , env = process.env.NODE_ENV || 'development';
+
+let logger = {};
 
 if (cluster.isMaster) {
-  let logger = childProcess.spawn('node'
-    , ['./node_modules/node-logging-stream/index.js']);
+  if(env === 'development') {
+    logger = childProcess.spawn('node'
+      , ['./node_modules/node-logging-stream/index.js']);
 
-  logger.stdout.on('data', function(data) {
-    console.log('Logger [out]: ' + data);
-  });
-  logger.stderr.on('data', function(data) {
-    console.log('Logger [err]: ' + data);
-  });
-  logger.on('close', function(code) {
-    console.log('Logger [close[: ' + code);
-  });
+    logger.stdout.on('data', function(data) {
+      console.log('Logger [out]: ' + data);
+    });
+    logger.stderr.on('data', function(data) {
+      console.log('Logger [err]: ' + data);
+    });
+    logger.on('close', function(code) {
+      console.log('Logger [close[: ' + code);
+    });
 
-  logger.stdin.write('[master] Start logging');
+    logger.stdin.write('[master] Start logging');
 
-  process.on('uncaughtException', err => {
-    logger.stdin.write(`[master] ${JSON.stringify(err)}`);
-  });
+    process.on('uncaughtException', err => {
+      logger.stdin.write(`[master] ${JSON.stringify(err)}`);
+    });
+  }
 
   for (var i = 0; i < workers; i++) {
     let worker = cluster.fork();
